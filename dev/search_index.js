cb5fcf4a32da397fe2b635cfa925f58eb7af12e7
Documenter.jl <documenter@juliadocs.github.io>
2024-09-16 02:30:52 +0000

    build based on eccc9d3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MattParkerJigsawPuzzleProblem","category":"page"},{"location":"#MattParkerJigsawPuzzleProblem","page":"Home","title":"MattParkerJigsawPuzzleProblem","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MattParkerJigsawPuzzleProblem.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MattParkerJigsawPuzzleProblem]","category":"page"}]
+}
