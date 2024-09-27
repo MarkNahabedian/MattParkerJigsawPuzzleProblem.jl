e1393ab136bc8da6e458926db2d53bdb1311e090
MarkNahabedian <naha@mit.edu>
2024-09-27 12:40:38 -0400

    terserep for MultipleSolutionPuzzle.

--- src/terse_representation.jl
+++ src/terse_representation.jl
@@ -10,19 +10,19 @@ edges, puzzle pieces and grids.
 
 =#
 
+
 terserep(et::EdgeType) = @sprintf("%d", et.uid)
 
 terserep(::Ball) = "b"
 terserep(::Socket) = "s"
+terserep(::Straight) = "_"
+
+terserep(::Nothing) = "?"
 
 terserep(edge::Edge) =
     @sprintf("%s%s",
              terserep(edge.edge_type),
-             if edge.edge_type.isperimeter
-                 "_"
-             else
-                 terserep(edge.bs)
-             end)
+             terserep(edge.bs))
 
 terserep(piece::AbstractPuzzlePiece) =
     join(terserep.(piece.edges), "/")
@@ -31,4 +31,30 @@ terserep(cell::GridCell) =
     join(terserep.([ get_edge(cell, direction)
                      for direction in CARDINAL_DIRECTIONS ]),
          "/")
+
+terserep(::Missing) = "#"
+
+function terserep(grid::Grid)
+    (nrows, ncols) = size(grid)
+    for r in 1:nrows
+        for c in 1:ncols
+            print(terserep(grid[r, c]), "\t")
+        end
+        println()
+    end
+end
+
+
+
+"""
+    terserep(puzzle)
+
+Output a terse printed representation of the puzzle.
+"""
+function terserep(puzzle::MultipleSolutionPuzzle)
+    for grid in puzzle.grids
+        terserep(grid)
+        println()
+    end
+end
 
