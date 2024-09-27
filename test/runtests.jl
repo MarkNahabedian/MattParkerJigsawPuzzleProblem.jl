e09063011f6f618752161df154cb737aeb590f7e
MarkNahabedian <naha@mit.edu>
2024-09-27 12:42:55 -0400

    Tests for edges_mate, ordering, get_neighboring_edge, MultipleSolutionPuzzle - single grid

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using MattParkerJigsawPuzzleProblem
 using Test
 
+@testset "edges_mate" begin
+    @test edges_mate(Edge(EdgeType(true, 0), Straight()),
+                     Edge(EdgeType(true, 1), Straight()))
+    @test edges_mate(Edge(EdgeType(false, 2), Ball()),
+                     Edge(EdgeType(false, 2), Socket()))
+    @test !edges_mate(Edge(EdgeType(false, 2), Ball()),
+                      Edge(EdgeType(false, 2), Ball()))
+    @test !edges_mate(Edge(EdgeType(false, 2), Ball()),
+                      Edge(EdgeType(false, 3), Socket()))
+end
+
 @testset "Cardinal directions" begin
     do_cardinal_directions() do dir
         @test opposite(opposite(dir)) == dir
@@ -23,6 +34,58 @@ using Test
     @test W()(2, 2) == [2, 1]
 end
 
+@testset "ordering" begin
+    @test isless(Edge(EdgeType(false, 2), Ball()),
+                 Edge(EdgeType(false, 3), Ball()),)
+    @test !isless(Edge(EdgeType(false, 3), Ball()),
+                  Edge(EdgeType(false, 2), Ball()),)
+    @test isless(
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 2), Ball()),
+                Edge(EdgeType(false, 3), Ball()),
+                Edge(EdgeType(false, 4), Ball()),
+                Edge(EdgeType(false, 5), Ball())
+            ])),
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 2), Ball()),
+                Edge(EdgeType(false, 5), Ball()),
+                Edge(EdgeType(false, 6), Ball()),
+                Edge(EdgeType(false, 7), Ball())
+            ])))
+    @test isless(
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 1), Ball()),
+                Edge(EdgeType(false, 3), Ball()),
+                Edge(EdgeType(false, 4), Ball()),
+                Edge(EdgeType(false, 5), Ball())
+            ])),
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 1), Ball()),
+                Edge(EdgeType(false, 5), Ball()),
+                Edge(EdgeType(false, 6), Ball()),
+                Edge(EdgeType(false, 7), Ball())
+            ])))
+    @test !isless(
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 5), Ball()),
+                Edge(EdgeType(false, 6), Ball()),
+                Edge(EdgeType(false, 7), Ball()),
+                Edge(EdgeType(false, 8), Ball())
+            ])),
+        ImmutablePuzzlePiece(
+            sort([
+                Edge(EdgeType(false, 1), Ball()),
+                Edge(EdgeType(false, 5), Ball()),
+                Edge(EdgeType(false, 6), Ball()),
+                Edge(EdgeType(false, 7), Ball())
+            ])))
+end
+
 @testset "GridCell operations" begin
     grid = new_grid(3, 3)
     for r in 1:3
@@ -40,9 +103,35 @@ end
     @test get_edge(grid[1, 1], S()) == e
 end
 
-@testset "MultipleSolutionPuzzle - single grid" begin
-    puzzle = MultipleSolutionPuzzle(3, 5, 1)
-    ## Verify for each grid that the perimeters are correct:
+@testset "get_neighboring_edge" begin
+    grid = new_grid(3, 3)
+    grid[1, 1] = GridCell(1, 1,
+                          ImmutablePuzzlePiece([
+                              Edge(EdgeType(true, 1), Straight()),
+                              Edge(EdgeType(false, 2), Ball()),
+                              Edge(EdgeType(false, 3), Socket()),
+                              Edge(EdgeType(true, 4), Straight())
+                          ]), 0)
+    ## Top left corner
+    @test get_neighboring_edge(grid, 1, 1, N()) ==
+        Edge(EdgeType(true, 0), Straight())
+    @test ismissing(get_neighboring_edge(grid, 1, 1, E()))
+    @test ismissing(get_neighboring_edge(grid, 1, 1, S()))
+    @test get_neighboring_edge(grid, 1, 1, W()) ==
+        Edge(EdgeType(true, 0), Straight())
+    ## Bottom right corner:
+    @test get_neighboring_edge(grid, 3, 3, E()) ==
+        Edge(EdgeType(true, 0), Straight())
+    @test get_neighboring_edge(grid, 3, 3, S()) ==
+        Edge(EdgeType(true, 0), Straight())
+    # Cell woth a real neighbor:
+    @test get_neighboring_edge(grid, 2, 1, N()) ==
+        Edge(EdgeType(false, 3), Socket())
+end
+
+
+## Verify for each grid that the perimeters are correct:
+function check_perimeters(puzzle::MultipleSolutionPuzzle)
     (nrows, ncols) = size(puzzle)
     for g in 1:length(puzzle.grids)
         grid = puzzle.grids[g]
@@ -62,7 +151,11 @@ end
             end
         end
     end
-    ## Verify for each grid that the edges mate
+end    
+
+## Verify for each grid that the edges mate
+function check_that_edges_mate(puzzle::MultipleSolutionPuzzle)
+    (nrows, ncols) = size(puzzle)
     for grid in puzzle.grids
         (nrows, ncols) = size(grid)
         for r in 1:nrows
@@ -83,23 +176,59 @@ end
     end
 end
 
+@testset "MultipleSolutionPuzzle - single grid" begin
+    puzzle = MultipleSolutionPuzzle(4, 5, 1)
+    check_perimeters(puzzle)
+    check_that_edges_mate(puzzle)
+    writing_html_file("single_grid_puzzle.html") do
+        grid_to_html.(puzzle.grids)
+    end
+end
+
+#=
+@testset "MultipleSolutionPuzzle - two grids" begin
+    puzzle = MultipleSolutionPuzzle(3, 5, 2)
+    check_perimeters(puzzle)
+    check_that_edges_mate(puzzle)
+end
+=#
+
+
 #=
 
 using MattParkerJigsawPuzzleProblem
 
-puzzle = MultipleSolutionPuzzle(3, 3, 1)
+begin
+    puzzle = MultipleSolutionPuzzle(4, 4, 1);
 
-terserep.(puzzle.grids[1])
+    solver = Solver(size(puzzle),
+                    map(ImmutablePuzzlePiece,
+                        puzzle_pieces(puzzle)));
+    grid = solver.working_grids[1]
+    next_empty_index = Tuple(findfirst(x -> !isa(x, GridCell), grid))
+    nothing
+end
 
- "1_/13s/14s/2_"   "3_/15b/16s/13b"   "4_/5_/17s/15s"
- "14b/18b/19b/6_"  "16b/20b/21s/18s"  "17b/7_/22b/20s"
- "19s/23b/8_/9_"   "21b/24s/10_/23s"  "22s/12_/11_/24b"
+grid = solver.working_grids[1]
+next_empty_index = Tuple(findfirst(x -> !isa(x, GridCell), grid))
+@assert next_empty_index == (2, 1)
+neighbor = N()(grid, next_empty_index...)
 
-ABOVE OK
+candidates = solver.edge_index[
+    edge_index_key(opposite(get_edge(neighbor, opposite(N()))))]
+c = candidates[1]
 
-pieces = ImmutablePuzzlePiece.(puzzle.grids[1])
+fit_piece(grid, next_empty_index..., c) do rot
+    println("*** $rot")
+end
 
-terserep.(pieces)
+
+add_one_piece(solver, first(solver.working_grids))
+
+writing_html_file("after_add_one_piece.html") do
+    grid_to_html.(solver.working_grids)
+end
+
 
 =#
 
