e1bc74b9f43d1dfbbad7e6f269e0250146da6709
MarkNahabedian <naha@mit.edu>
2024-09-27 12:40:47 -0400

    writing_html_file, surround each grid with border.

--- src/grid_html.jl
+++ src/grid_html.jl
@@ -1,6 +1,7 @@
+import XML
 using XML: Element
 
-export html_wrapper, grid_to_html
+export writing_html_file, html_wrapper, grid_to_html
 
 #=
 
@@ -11,6 +12,12 @@ generate an HTML table from a puzzle grid.
 
 GRID_STYESHEET = """
 
+.grid {
+    display: block;
+    margin: 2ex;
+    padding: 2ex;
+    border: solid yellow;
+}
 .GridCell {
     display: block;
     margin: 0;
@@ -44,6 +51,13 @@ GRID_STYESHEET = """
 }
 """
 
+function writing_html_file(body, filename)
+    ## body should return a vector of HTML Elements.
+    XML.write(filename,
+              html_wrapper(body());
+              indentsize=2)
+end
+
 function html_wrapper(body_elements)
     Element(
         "html",
@@ -59,32 +73,41 @@ end
 function grid_to_html(grid::Grid)
     (nrows, ncols) = size(grid)
     Element(
-    "table",
-    [ Element(
-        "tr",
-        [
-            Element(
-                "td",
-                Element(
-                    "div",
-                    let
-                        cell = grid[row, col]
-                        edges = []
-                        for d in [N(), W(), E(), S()]
-                            edge = get_edge(cell, d)
-                            push!(edges,
-                                  Element(
-                                      "div",
-                                      terserep(edge);
-                                      class="Edge $(string(typeof(d)))"))
-                        end
-                        edges
-                    end...;
-                    class="GridCell")
-            )
-            for col in 1:ncols
-                ]...
-    )
-      for row in 1:nrows ]...)
+        "div",
+        Element(
+            "table",
+            [ Element(
+                "tr",
+                [
+                    Element(
+                        "td",
+                        Element(
+                            "div",
+                            let
+                                cell = grid[row, col]
+                                if cell isa GridCell
+                                    edges = []
+                                    for d in [N(), W(), E(), S()]
+                                        edge = get_edge(cell, d)
+                                        ## Maybe do something to highlight
+                                        ## mismatched edges.
+                                        push!(edges,
+                                              Element(
+                                                  "div",
+                                                  terserep(edge);
+                                                  class="Edge $(string(typeof(d)))"))
+                                    end
+                                    edges
+                                else
+                                    [ "&nbsp" ]
+                                end
+                            end...;
+                            class="GridCell")
+                    )
+                    for col in 1:ncols
+                        ]...
+                            )
+              for row in 1:nrows ]...);
+    class="grid")
 end
 
